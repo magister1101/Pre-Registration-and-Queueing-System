edeee900e41389c26855f9e362f96978b02cf738
MarkVanMortem <34596876+magister1101@users.noreply.github.com>
2025-01-16 12:33:37 +0800

    get queues

--- api/controllers/queues.js
+++ api/controllers/queues.js
@@ -1,3 +1,61 @@
 const mongoose = require('mongoose');
 
 const Queue = require('../models/queue');
+
+exports.getQueues = async (req, res) => {
+    try {
+        const { isArchived, query, filter } = req.query;
+
+        const escapeRegex = (value) => {
+            return value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        };
+
+        let searchCriteria = {};
+        const queryConditions = [];
+
+        if (query) {
+            const escapedQuery = escapeRegex(query);
+            const orConditions = [];
+
+            if (mongoose.Types.ObjectId.isValid(query)) {
+                orConditions.push({ _id: query });
+            }
+            orConditions.push(
+                { queueNumber: { $regex: escapedQuery, $options: 'i' } },
+                { student: { $regex: escapedQuery, $options: 'i' } },
+                { description: { $regex: escapedQuery, $options: 'i' } },
+            );
+
+            queryConditions.push({ $or: orConditions });
+        }
+
+        if (filter) {
+            const escapedFilter = escapeRegex(filter);
+            queryConditions.push({
+                $or: [
+                    { status: { $regex: escapedFilter, $options: 'i' } },
+                    { destination: { $regex: escapedFilter, $options: 'i' } },
+                ],
+            });
+        }
+
+        if (isArchived) {
+            const isArchivedBool = isArchived === 'true'; // Convert to boolean
+            queryConditions.push({ isArchived: isArchivedBool });
+        }
+
+        if (queryConditions.length > 0) {
+            searchCriteria = { $and: queryConditions };
+        }
+        const queues = await Queue.find(searchCriteria);
+
+        return res.status(200).json(queues);
+
+    } catch (error) {
+        console.error('Error retrieving queues:', error);
+        return res.status(500).json({
+            message: "Error in retrieving queues",
+            error: error.message || error,
+        });
+    }
+};
